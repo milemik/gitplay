d6c4b462df9064b83905eebdcd4661529d2f3fd7
Ivan Miletic <milemik68@gmail.com>
2021-09-26 15:34:52 +0200

    Added test case for empty list

--- common/common.py
+++ common/common.py
@@ -2,4 +2,6 @@ from typing import Union
 
 
 def get_first(values: list) -> Union[str, int]:
+    if not values:
+        return None
     return values[0]
--- test_zdravo.py
+++ test_zdravo.py
@@ -4,3 +4,7 @@ from common.common import get_first
 def test_get_first():
     values = [1, 2, 3]
     assert get_first(values=values) == 1
+
+
+def test_get_first_empty_list():
+    assert get_first(values=[]) is None
