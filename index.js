6234fe8756484fb3afb2733730a91fcf23d7a112
milemik <milemik68@gmail.com>
2021-11-24 22:38:44 +0100

    Change on dev directly - js file

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let cur_date = Date()
 document.getElementById("message").innerHTML = cur_date
 
-console.log("Hello world")§
+console.log("Hello world")
 document.getElementById("hello").innerHTML = "who?"
+
+
+console.log("Change on dev")
