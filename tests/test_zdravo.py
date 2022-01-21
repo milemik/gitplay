0ead94d9d3de8c43d860912ca298ea576db2aab4
milemik <milemik68@gmail.com>
2022-01-21 11:41:01 +0100

    Black format test

--- tests/test_zdravo.py
+++ tests/test_zdravo.py
@@ -2,9 +2,12 @@ import pytest
 
 from common.common import get_first
 from models.user import User
+
+
 def test_get_first():
     values = [1, 2, 3]
     assert get_first(values=values) == 1
+
 
 def test_get_first_empty_list():
     assert get_first(values=[]) is None
