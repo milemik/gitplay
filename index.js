5e170f7f24366121d2e2f1f0f1dcb80c0e1616c0
Ivan Miletic <milemik68@gmail.com>
2024-04-19 10:48:01 +0200

    Added function getUserLastName

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const cur_date = Date()
+const cur_date = Date();
 
 // Will add current date to message
 function updateHtml() {
-    document.getElementById("message").innerHTML = cur_date;
+  document.getElementById("message").innerHTML = cur_date;
 
-    console.log("Hello world");
-    document.getElementById("hello").innerHTML = "who?";
+  console.log("Hello world");
+  document.getElementById("hello").innerHTML = "who?";
 }
 
 updateHtml();
 
-
 function addUser() {
-    console.log("User added");
+  console.log("User added");
 }
 
-
+function getUserLastName() {
+  return "LAST NAME";
+}
