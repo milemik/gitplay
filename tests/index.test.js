198e6766a38dad07772597d17f0f70f62ed1a991
Ivan Miletic <milemik68@gmail.com>
2024-08-13 21:34:50 +0200

    Created test config
    - Need to fix test issue with jest

---
+++ tests/index.test.js
@@ -0,0 +1,24 @@
+/**
+ * @jest-environment jsdom
+ */
+const assert = require("assert");
+const { addUser } = require("../index.js");
+
+
+describe("addUser", () => {
+    beforeEach(() => {
+        document.body.innerHTML = `
+        <div class="wrapper">
+            <h1>GitPlay</h1>
+            <h5>Hello World</h5>
+            <p id="message"></p>
+            <p id="hello"></p>
+            <p>By Mile</p>
+        </div>
+        `
+    })
+    test("add user", () => {
+        let res = addUser();
+        assert.stringEqual(res, "");
+    });
+});
